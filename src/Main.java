470b0d8e48575cb6b0af3f6afda6e5f1c2345917
AdrianVT <35340183+AdrianVT@users.noreply.github.com>
2023-11-23 11:52:52 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,55 @@
+import itumulator.executable.DisplayInformation;
 import itumulator.executable.Program;
+import itumulator.world.Location;
+import itumulator.world.World;
+
+import java.awt.*;
+import java.util.Random;
+
 
 public class Main {
 
-    public static void main(String[] args) {
 
-      
+    public static void main(String[] args) {
+        int size = 15; // størrelsen af vores 'map' (dette er altid kvadratisk)
+        int delay = 1000; // forsinkelsen mellem hver skridt af simulationen (i ms)
+        int display_size = 800; // skærm opløsningen (i px)
+
+        Program p = new Program(size, display_size, delay); // opret et nyt program
+        World world = p.getWorld(); // hiv verdenen ud, som er der hvor vi skal tilføje ting!
+
+        // how they look
+        DisplayInformation di = new DisplayInformation(Color.red);
+        p.setDisplayInformation(Person.class, di);
+
+
+        Person person = new Person();
+        Location place = new Location(0,1);
+        world.setTile(place, person);
+
+        int amount = 10;
+        Random r = new Random();
+        for(int i = 0; i < amount; i++) {
+            int x = r.nextInt(size);
+            int y = r.nextInt(size);
+            Location l = new Location(x, y);
+            // Så længe pladsen ikke er tom, forsøger vi med en ny tilfældig plads:
+            while (!world.isTileEmpty(l)) {
+                x = r.nextInt(size);
+                y = r.nextInt(size);
+                l = new Location(x, y);
+            }
+            // og herefter kan vi så anvende den:
+            world.setTile(l, new Person());
+        }
+        p.show(); // viser selve simulationen
+        p.simulate();
+
+
+
+
+
+
     }
+
 }
---
+++ src/Person.java
@@ -0,0 +1,23 @@
+import itumulator.simulator.Actor;
+import itumulator.world.Location;
+import itumulator.world.World;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.Random;
+public class Person implements Actor {
+
+    @Override
+    public void act(World world) {
+        Set<Location> neighbours = world.getEmptySurroundingTiles();
+        List<Location> list = new ArrayList<>(neighbours);
+        Location l = list.get(0); // Linje 2 og 3 kan erstattes af neighbours.toArray()[0]
+        world.move(this,l);
+
+        if(world.isNight()) {
+            world.delete(this);
+        }
+
+    }
+
+}
